19059c13a10f1f4cf6df92d60dec6478370a5524
João Paulo <johnowl@gmail.com>
2019-07-27 10:22:26 -0300

    Add /positions in root path of PositionController.

--- src/main/kotlin/com/johnowl/services/locations/controller/PositionController.kt
+++ src/main/kotlin/com/johnowl/services/locations/controller/PositionController.kt
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
-@RestController
+@RestController("/positions")
 class PositionController {
 
     private val positionService: PositionService
