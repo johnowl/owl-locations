5c46c381598667cdd404686830911e0f349b9024
João Paulo <johnowl@gmail.com>
2019-07-29 18:42:24 -0300

    Remove compilation warning.

--- src/main/kotlin/com/johnowl/services/locations/model/PositionRepository.kt
+++ src/main/kotlin/com/johnowl/services/locations/model/PositionRepository.kt
@@ -6,5 +6,5 @@ interface PositionRepository {
     fun save(position: Position): Position
     fun findFirstByReferenceOrderByCreatedAtDesc(reference: String): Position
     fun getByReferenceOrderByCreatedAtDesc(reference: String): List<Position>
-    fun findAllNear(latitude: Double, longitude: Double, maxDistance: Double): List<Position>
+    fun findAllNear(latitude: Double, longitude: Double, maxDistanceInMeters: Double): List<Position>
 }
